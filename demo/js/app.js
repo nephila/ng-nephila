480c9a137570fe4b6a8bb91211ad5f3d6b833739
Andrea Stagi <stagi.andrea@gmail.com>
2015-08-17 13:26:27 +0200

    Update demo

--- demo/js/app.js
+++ demo/js/app.js
@@ -7,9 +7,15 @@ app.config(function(paginationProvider) {
 app.controller('demoCtrl', function($scope, $timeout, pagination) {
   $scope.myText = 'the last good day of the year';
   $scope.loadCount = 0;
+  $scope.loadingShow = true;
+  $scope.scrollContainer = document.getElementById('innerscroll');
+  console.log($scope.scrollContainer);
   $scope.loadMore = function() {
     alert("Load more!");
     $scope.loadCount++;
+    if ($scope.loadCount==2) {
+      $scope.loadingShow = false;
+    }
   }
   $scope.moreDataCanBeLoaded = function() {
     return $scope.loadCount < 2;
@@ -17,6 +23,7 @@ app.controller('demoCtrl', function($scope, $timeout, pagination) {
   $scope.pageChange = function(page) {
     $timeout(function() {
       $scope.numberOfItems = 100;
+      $scope.loadingShow = false;
       alert("BAH, " + page);
     }, 1000);
   }
