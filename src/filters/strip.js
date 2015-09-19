67c701dd4e2b832dee3c5c78dc96734aec7911e5
Andrea Stagi <stagi.andrea@gmail.com>
2015-09-20 00:20:41 +0100

    Add strip

---
+++ src/filters/strip.js
@@ -0,0 +1,13 @@
+angular.module('ngNephila.filters.strip', [])
+.filter('strip', function() {
+  return function(s, ch) {
+    ch = (ch || ' ');
+    while (s.length > 0 && ch.indexOf(s.charAt(0)) != -1) {
+      s = s.substr(1);
+    }
+    while (s.length > 0 && ch.indexOf(s.charAt(s.length - 1)) != -1) {
+      s = s.substr(0, s.length - 1);
+    }
+    return s;
+  };
+});
---
+++ src/filters/test/strip.spec.js
@@ -0,0 +1,22 @@
+describe('Filter: strip', function () {
+
+  beforeEach(module('ngNephila.filters.strip'));
+
+  var stripFilter;
+  beforeEach(inject(function($filter) {
+    stripFilter = $filter('strip');
+  }));
+
+  it('has a strip filter', function () {
+    expect(stripFilter).not.toBeNull();
+  });
+
+  it('should strip string', function () {
+    expect(stripFilter('//comment//', '/')).toEqual('comment');
+    expect(stripFilter('/comment//', '/')).toEqual('comment');
+    expect(stripFilter('  too many spaces ')).toEqual('too many spaces');
+    expect(stripFilter('Hello!', '/')).toEqual('Hello!');
+    expect(stripFilter('Hello!', '!')).toEqual('Hello');
+  });
+
+});
