25e97a95cdb967db9698b7ffc8c1059903545269
Andrea Stagi <stagi.andrea@gmail.com>
2015-09-21 00:45:44 +0100

    Add pathJoin service

--- src/services/module.js
+++ src/services/module.js
@@ -2,5 +2,6 @@ angular.module('ngNephila.services', [
   'ngNephila.services.pagination',
   'ngNephila.services.debounce',
   'ngNephila.services.scrolledInContainer',
-  'ngNephila.services.tts'
+  'ngNephila.services.tts',
+  'ngNephila.services.pathJoin'
 ]);
